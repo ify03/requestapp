7495deae1543452525cdd6f59ebea0b16f433306
ify03 <blessingifebuche200@gmail.com>
2023-07-31 13:56:40 +0100

    adding assets

---
+++ lib/page/finder.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class TheBody extends StatelessWidget {
+  const TheBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Image.asset('requestapp/images/images.jpeg'),
+          const Padding(
+            padding: EdgeInsets.all(28.0),
+            child: Text(
+              'No New Request',
+              maxLines: 2,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const Text(
+              'When people send you a friend request,\n they\'ll appear here.'),
+          const SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.blue)),
+            onPressed: () {
+              print('Pressed');
+            },
+            child: const Text('See Friend Request'),
+          )
+        ],
+      ),
+    );
+  }
+}
